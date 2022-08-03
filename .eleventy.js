1d67a353176cfcf4899dcc4c4c86830010d37c88
Wouter De Moor <mreezeh@gmail.com>
2022-08-03 18:36:06 +0200

    Update mail and projectcardvideo

--- .eleventy.js
+++ .eleventy.js
@@ -60,13 +60,26 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addShortcode(
         "projectcard",
         function (title, date, subtitle, linkHref, imageSource) {
-            return `<section class="project-cell">
-      <a href="${linkHref}" target="_blank" rel="noopener noreferrer">
-        <img src="${imageSource}" alt="${title} cover">
-            <h3>${title} &Iota; ${date}</h3>
-        <div>${subtitle}</div>
-      </a>
-    </section>`;
+            return `
+            <section class="project-cell">
+                <a href="${linkHref}" target="_blank" rel="noopener noreferrer">
+                    <img src="${imageSource}" alt="${title} cover">
+                    <h3>${title} &Iota; ${date}</h3>
+                    <div>${subtitle}</div>
+                </a>
+            </section>`;
+        }
+    );
+
+    eleventyConfig.addShortcode(
+        "projectcardvideo",
+        function (title, date, subtitle, videoSource) {
+            return `
+            <section class="project-cell">
+                <iframe src="${videoSource}"></iframe>
+                <h3>${title} &Iota; ${date}</h3>
+                <div>${subtitle}</div>
+            </section>`;
         }
     );
 
